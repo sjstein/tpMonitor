8cee903b79f2a191eedf7a38cf93510490ccde9d
sjstein <s.joshua.stein@gmail.com>
2020-03-23 13:57:30 -0500

    Cleaned up exception handling. Added option to loop forever.

--- tpMonitor.py
+++ tpMonitor.py
@@ -5,7 +5,7 @@ import time
 import sys
 
 
-archiveFreq = 30    # Default archive frequency in seconds
+archiveFreq = 5     # Default archive frequency in seconds
 runTime = 60        # Default run time in minues
 logging = False     # Default to no logging
 PORT = 5005         # TCP port for connection to server
@@ -18,32 +18,38 @@ parser.add_argument("serverIP", help="IP Number of server")
 parser.add_argument("-l", "--log", help="File name for logging (default is NO logging)")
 parser.add_argument("-f", "--freq", help="Frequency (in secs) to read data [default " + str(archiveFreq) + " sec]",\
                     type=int)
-parser.add_argument("-t", "--time", help="Time (in mins) to run [default " + str(runTime) + " min ]", type=int)
+parser.add_argument("-t", "--time", help="Time (in mins) to run [default " + str(runTime) + " min ]. -1 denotes run\
+ forever.", type=int)
 
 # Read arguments passed on command line
 args = parser.parse_args()
+fname = ''                      # filename to log to
+f = ''                          # file object
+data = ''                       # Return string from server
+cTime = ''
+cDate = ''
+depth = 0.0
+depthFt = 0.0
 
 # Parse command line arguments
 serverAddr = args.serverIP      # Server IP # - not optional
 if args.log:                    # Log filename - optional
     fname = args.log
     logging = True
-if args.freq:                # Read frequency - optional (default defined above)
+if args.freq:                   # Read frequency - optional (default defined above)
     archiveFreq = args.freq
 if args.time:                   # Run duration - optional (default defined above)
     runTime = args.time
 
 # Display summary of command on console
-print "Connecting to server at :" + serverAddr
+print("Connecting to server at :" + serverAddr)
 if logging:
     # Open the file and write the header
     f = open(fname, 'a')
     f.write("Date Time,Press(mBar),Temp(c),Depth(m)\n")
 else:
-    print "Not logging to a file"
-print "Reading data every " + str(archiveFreq) + " seconds"
-print "Acquiring data for " + str(runTime) + " minutes"
-
+    print("Not logging to a file")
+print("Reading data every " + str(archiveFreq) + " seconds")
 
 
 # Write status to console
@@ -52,23 +58,27 @@ if logging:
     print("Saving to file     : " + fname)
 print("Server IP#         : " + serverAddr)
 print("Logging frequency  : " + str(archiveFreq) + " seconds")
-print("Acquiring data for : " + str(runTime) + " minutes")
+if runTime <= 0:
+    print("Acquiring data until stopped via user interrupt (ctrl-c)")
+else:
+    print("Acquiring data for : " + str(runTime) + " minutes")
 currentDT = datetime.datetime.now()
-print  # blank line
-print("Acquisition started :" + str(currentDT.strftime("%Y%m%d")) + " at " + str(currentDT.strftime("%H:%M:%S")))
+print   # blank line
+print("Acquisition started : " + str(currentDT.strftime("%Y%m%d")) + " at " + str(currentDT.strftime("%H:%M:%S")))
+print("---------------------")
 
 # Main loop
 while True:
-    if logging:
-        f = open(fname, 'a')
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(5) # Set timeout for no response from server
-    s.connect((serverAddr, PORT))
-    currentDT = datetime.datetime.now()
     try:
+        if logging:
+            f = open(fname, 'a')
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(5)     # Set timeout for no response from server
+        s.connect((serverAddr, PORT))
+        currentDT = datetime.datetime.now()
         data = s.recv(1024)
         # populate list with elements of pressure, temperature
-        elem = data.split(",")  # type: List[str]
+        elem = data.split(",")      # type : List[str]
         depth = elem[2]
         depthFt = float(depth) * 3.28084
         cDate = str(currentDT.strftime("%Y%m%d"))
@@ -76,26 +86,30 @@ while True:
         dataLine = cDate + ' ' + cTime + ',' + str(data)
         if logging:
             f.write(dataLine + '\n')
+        print("Server reports : " + data + " at " + cTime + "(" + cDate + ")")
+        if runTime <= 0:
+            print("Run time       : " + str(accumulatedTime) + " seconds")
+        else:
+            print("Run time       : " + str(accumulatedTime) + " of " + str(int(runTime) * 60) + " seconds")
+        print("Current depth  : " + "{0:.2f}".format(float(depth)) + " meters (" + "{0:.2f}".format(
+            depthFt) + " ft)\n")
+        if logging:
+            f.close()
+            s.close()
+        time.sleep(float(archiveFreq))
+        accumulatedTime = accumulatedTime + float(archiveFreq)
+        if (runTime > 0) & (accumulatedTime > int(runTime) * 60):
+            print("Acquisition complete")
+            exit(0)
     except socket.timeout:
         print("ERR: Timeout waiting for server response")
     except KeyboardInterrupt:
         if logging:
             f.close()
             s.close()
-        print("Acquisition terminated via user interrupt")
+        print("tpMonitor terminated via user interrupt")
         exit(0)
-    finally:
-        print("Server reports : " + data + " at " + cTime + "(" + cDate + ")")
-        print("Run time       : " + str(accumulatedTime) + " of " + str(int(runTime) * 60) + " seconds")
-        print("Current depth  : " + "{0:.2f}".format(float(depth)) + " meters (" + "{0:.2f}".format(depthFt) + " ft)\n")
-        if logging:
-            f.close()
-            s.close()
-        time.sleep(float(archiveFreq))
-        accumulatedTime = accumulatedTime + float(archiveFreq)
-        if accumulatedTime > int(runTime) * 60:
-            print("Acquisition complete")
-            exit(0)
+
 
 
 
