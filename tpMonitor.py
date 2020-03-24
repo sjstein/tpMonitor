598bdec5044ea5cc2a5da1727047722a8836df7e
sjstein <s.joshua.stein@gmail.com>
2020-03-24 09:50:34 -0500

    Updated to honor Python 3.8 syntax

--- tpMonitor.py
+++ tpMonitor.py
@@ -42,29 +42,29 @@ if args.time:                   # Run duration - optional (default defined above
     runTime = args.time
 
 # Display summary of command on console
-print("Connecting to server at :" + serverAddr)
+print(("Connecting to server at :" + serverAddr))
 if logging:
     # Open the file and write the header
     f = open(fname, 'a')
     f.write("Date Time,Press(mBar),Temp(c),Depth(m)\n")
 else:
     print("Not logging to a file")
-print("Reading data every " + str(archiveFreq) + " seconds")
+print(("Reading data every " + str(archiveFreq) + " seconds"))
 
 
 # Write status to console
 print("Starting pressure and temperature log with the following parameters:")
 if logging:
-    print("Saving to file     : " + fname)
-print("Server IP#         : " + serverAddr)
-print("Logging frequency  : " + str(archiveFreq) + " seconds")
+    print(("Saving to file     : " + fname))
+print(("Server IP#         : " + serverAddr))
+print(("Logging frequency  : " + str(archiveFreq) + " seconds"))
 if runTime <= 0:
     print("Acquiring data until stopped via user interrupt (ctrl-c)")
 else:
-    print("Acquiring data for : " + str(runTime) + " minutes")
+    print(("Acquiring data for : " + str(runTime) + " minutes"))
 currentDT = datetime.datetime.now()
-print   # blank line
-print("Acquisition started : " + str(currentDT.strftime("%Y%m%d")) + " at " + str(currentDT.strftime("%H:%M:%S")))
+print("")   # blank line
+print(("Acquisition started : " + str(currentDT.strftime("%Y%m%d")) + " at " + str(currentDT.strftime("%H:%M:%S"))))
 print("---------------------")
 
 # Main loop
@@ -78,21 +78,21 @@ while True:
         currentDT = datetime.datetime.now()
         data = s.recv(1024)
         # populate list with elements of pressure, temperature
-        elem = data.split(",")      # type : List[str]
+        elem = data.split(b',')      # type : List[str]
         depth = elem[2]
         depthFt = float(depth) * 3.28084
         cDate = str(currentDT.strftime("%Y%m%d"))
         cTime = str(currentDT.strftime("%H:%M:%S"))
-        dataLine = cDate + ' ' + cTime + ',' + str(data)
+        dataLine = cDate + ' ' + cTime + ',' + data.decode("utf-8")
         if logging:
             f.write(dataLine + '\n')
-        print("Server reports : " + data + " at " + cTime + "(" + cDate + ")")
+        print(("Server reports : " + data.decode("utf-8") + " at " + cTime + "(" + cDate + ")"))
         if runTime <= 0:
-            print("Run time       : " + str(accumulatedTime) + " seconds")
+            print(("Run time       : " + str(accumulatedTime) + " seconds"))
         else:
-            print("Run time       : " + str(accumulatedTime) + " of " + str(int(runTime) * 60) + " seconds")
-        print("Current depth  : " + "{0:.2f}".format(float(depth)) + " meters (" + "{0:.2f}".format(
-            depthFt) + " ft)\n")
+            print(("Run time       : " + str(accumulatedTime) + " of " + str(int(runTime) * 60) + " seconds"))
+        print(("Current depth  : " + "{0:.2f}".format(float(depth)) + " meters (" + "{0:.2f}".format(
+            depthFt) + " ft)\n"))
         if logging:
             f.close()
             s.close()
